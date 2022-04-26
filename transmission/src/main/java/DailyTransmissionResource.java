d37bb7cd113fff4ae2f22163bc5d6413cff5f3ab
IT20217518 <88072076+IT20217518@users.noreply.github.com>
2022-04-26 14:19:24 +0530

    Update DailyTransmissionResource.java

--- transmission/src/main/java/DailyTransmissionResource.java
+++ transmission/src/main/java/DailyTransmissionResource.java
@@ -16,34 +16,7 @@ import jakarta.ws.rs.core.MediaType;
 
 	    }
 
-	    // post method
-	   @POST
-	    @Path("/dailytransmission")
-	   @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
-	    public DailyTransmission createDailyTransmission(DailyTransmission los){
-	       repo.insertDailyTransmission(los);
-	       return los;
-	    }
-
-	    //post method
-	    @PUT
-	    @Path("/dailytransmission")
-	    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
-	    public DailyTransmission updateDailyTransmission(DailyTransmission los){
-	        repo.updateDailyTransmission(los);
-	        return los;
-	    }
-
-	    //delete method
-	    @DELETE
-	    @Path("dailytransmission/{daiTransId}")
-	    public DailyTransmission killDailyTransmission(@PathParam("daiTransId") int daiTransId){
-	    	DailyTransmission los = new DailyTransmission();
-
-	        if(los.getDaiTransId()!=0)
-	            repo.deleteDailyTransmission(daiTransId);
-	        return los;
-	    }
+	
 
 	}
 
