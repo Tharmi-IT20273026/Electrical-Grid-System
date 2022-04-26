11180eb6a3bedde33dddd8c8ad8d100c0e0bc8ed
IT20217518 <88072076+IT20217518@users.noreply.github.com>
2022-04-26 14:21:20 +0530

    Update TransmissionResource.java

--- transmission/src/main/java/TransmissionResource.java
+++ transmission/src/main/java/TransmissionResource.java
@@ -16,33 +16,17 @@ public class TransmissionResource {
 
     }
 
-    // post method
-   @POST
-    @Path("/transmission")
-   @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
-    public Transmission createTransmission(Transmission los){
-       repo.insertTransmission(los);
-       return los;
-    }
+  
 
     //post method
     @PUT
     @Path("/transmission")
     @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
     public Transmission updateTransmission(Transmission los){
-        repo.updateTransmission(los);
+        repo.updatetransmission(los);
         return los;
     }
 
-    //delete method
-    @DELETE
-    @Path("transmission/{transSubId}")
-    public Transmission killTransmission(@PathParam("transSubId") int transSubId){
-    	Transmission los = new Transmission();
-
-        if(los.getTransSubId()!=0)
-            repo.deleteTransmission(transSubId);
-        return los;
-    }
+   
 
 }
